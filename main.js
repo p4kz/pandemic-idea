5083b5102e33de1f573f45d2ce919bce76feebef
p4kz <wolpak@hotmail.com>
2022-03-26 06:34:51 -0300

    create all pages

--- main.js
+++ main.js
@@ -4,4 +4,13 @@
         .querySelector('#modal')
         .classList
         .toggle('hide')
+
+        document
+        .querySelector('#modal')
+        .classList
+        .toggle('hideScroll')
+
+        document.querySelector('#modal')
+        .classList
+        .toggle('addScroll')
     }
